1693f5fb5836c990d5ca92e6d485ce2f8a53941a
DaeHyeon Jang <161687815+JangDaeHyeon@users.noreply.github.com>
2025-02-03 15:28:19 +0900

    Update gemini_chatbot_streamlit.py

--- gemini_chatbot_streamlit.py
+++ gemini_chatbot_streamlit.py
@@ -11,7 +11,7 @@ API_KEY = os.getenv('GOOGLE_GEMINI_API_KEY')
 genai.configure(api_key=API_KEY)
 
 # Streamlit 앱 타이틀 설정
-st.title("스마트팜 생육정보 챗봇")
+st.title("전주대학교 인공지능 학과 챗봇")
 
 # JSON 파일에서 FAQ 데이터 불러오기
 def load_faq_data(file_path):
@@ -20,7 +20,7 @@ def load_faq_data(file_path):
     return faq_data
 
 # FAQ 데이터 불러오기
-faq_data = load_faq_data('smartfarm.json')
+faq_data = load_faq_data('faq_data.json')
 
 # 세션 상태 초기화
 if "messages" not in st.session_state:
@@ -39,7 +39,7 @@ def ask_question_with_context(question):
     return response.text
 
 # 질문 입력 및 응답 생성
-if prompt := st.chat_input("스마트팜 생육정보에 대해 궁금한 것을 입력하세요:"):
+if prompt := st.chat_input("전주대학교 인공지능 학과에 대해 궁금한 것을 입력하세요:"):
     # 사용자 메시지를 세션 상태에 추가
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
